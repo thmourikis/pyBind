85b0face2604cba165a7498234f3113813622e12
Thanos Mourikis <athanasios.mourikis@kcl.ac.uk>
2017-02-04 19:40:43 +0000

    Pseudo-parallel running and new ui

--- pyBind.py
+++ pyBind.py
@@ -5,11 +5,16 @@ from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QWidget, QMessageBox, QApplication, QDesktopWidget, QFileDialog
 import subprocess
+import math
+from os.path import expanduser
+import time
 
 class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
         uic.loadUi('mainwindow.ui', self)
+        self.inputPath = None
+        self.outPath = None
         self.initUI()
 
     def initUI(self):
@@ -23,26 +28,26 @@ class MyWindow(QtWidgets.QMainWindow):
         self.runbtn.clicked.connect(self.runNetMHCpan)
 
     def runNetMHCpan(self):
-        als = self.inputHLAallele.selectedItems()
-        als = [x.text() for x in als]
-        print(als)
+
+        ##Print a message that the run stated
+
+        home = expanduser("~")
+        hlas = self.inputHLAallele.selectedItems()
+        hlas = [x.text() for x in hlas]
+        print(hlas)
         mers = self.inputMers.selectedItems()
         mers = [x.text() for x in mers]
         print(mers)
         seq = self.inputText.toPlainText()
         print(seq)
 
-        ## Create all different peptides around mutations of interest
-        #a = 'abcdefg'
-        #b = [a[i:i + 3] for i in xrange(len(a) - 2)]
-
 
         ## After taking all parameters I need to update the configuration script of netMHCpan
         netConfigFile = os.getcwd() + "/resources/netMHCpan-3.0/netMHCpan"
         new_config = os.getcwd() + "/resources/netMHCpan-3.0/netMHCpan_new"
 
-        if not os.path.exists(os.getcwd()+"/scratch"):
-            os.makedirs(os.getcwd()+"/scratch")
+        if not os.path.exists(home+"/scratch"):
+            os.makedirs(home+"/scratch")
 
         with open(new_config, 'w') as new_file:
             with open(netConfigFile) as old_file:
@@ -51,7 +56,7 @@ class MyWindow(QtWidgets.QMainWindow):
                         line = "setenv NMHOME " + os.getcwd() + "/resources/netMHCpan-3.0\n"
                         new_file.write(line)
                     elif "setenv TMPDIR" in line:
-                        line = "\tsetenv TMPDIR " + os.getcwd() + "/scratch\n"
+                        line = "\tsetenv TMPDIR " + home + "/scratch\n"
                         new_file.write(line)
                     else:
                         new_file.write(line)
@@ -62,11 +67,48 @@ class MyWindow(QtWidgets.QMainWindow):
         st = os.stat(new_config[:-4])
         os.chmod(new_config[:-4], st.st_mode | stat.S_IEXEC)
 
-        cmd = '{} -p {} -a HLA-A02:01,HLA-A02:02 > {}'.format(
-            netConfigFile, os.getcwd() + "/resources/netMHCpan-3.0/test/test.pep",
-            os.getcwd() + "/resources/netMHCpan-3.0/test/test.pep.myout2")
-        output = subprocess.call('{}'.format(cmd), shell=True)
-        #print(str(output).split("\n"))
+        ## run job in chunks - pseudo-parallel
+        threads=3
+        chunk_cut = 2
+        chunks = math.ceil(len(hlas)/chunk_cut)
+
+        pids = []
+        completed_output_handles = []
+        commands = []
+        for i in range(0, chunks):
+            hlasToRun = ",".join(hlas[(i*chunk_cut):(i*chunk_cut)+chunk_cut])
+            cmd = '{} -p {} -a {}'.format(
+                netConfigFile, os.getcwd() + "/resources/netMHCpan-3.0/test/test.pep",
+                               hlasToRun)
+            command = cmd.split()
+            output_fn = 'netMHCpan.myout' + str(i)
+            commands.append((command, output_fn))
+
+        for i in range(0, threads):
+            if len(commands) != 0:  ## still stuff left to execute:
+                new_command, new_output_fn = commands.pop(0)
+                new_output_handle = open(new_output_fn, 'w')
+                new_pid = subprocess.Popen(new_command, stdout=new_output_handle, stderr=subprocess.PIPE)
+                pids.append((new_pid, new_output_handle))
+
+        while len(pids) != 0:
+            time.sleep(1)
+            for (pid, output_handle) in pids:  ## check running processes
+                pid_return = pid.poll()
+                ## poll returns None until the process finishes, then it returns the exit code
+                if (pid_return != 0 and pid_return != None):
+                    exit("Error: exonerate failed (code %d): %s" % (pid_return, pid.stderr))
+                elif pid_return == 0:  ## Successful completion
+                    output_handle.close()
+                    completed_output_handles.append(output_handle)
+                    pids.remove((pid, output_handle))
+                    if len(commands) != 0:  ## still stuff left to execute:
+                        new_command, new_output_fn = commands.pop(0)
+                        new_output_handle = open(new_output_fn, 'w')
+                        new_pid = subprocess.Popen(new_command, stdout=new_output_handle, stderr=subprocess.PIPE)
+                        pids.append((new_pid, new_output_handle))
+
+        assert (len(completed_output_handles) == chunks)
 
     def get_HLA_alleles(self):
         hla_cat = str(self.inputHLACategory.currentText())
